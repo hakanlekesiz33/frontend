9a603eb4cf27884e3c1d184b3ab181e7efe58570
Hakan LEKESİZ <hakan.lekesiz@baogrenci.com>
2022-10-30 11:33:09 +0300

    3. ders

--- HTML/content/js/base.js
+++ HTML/content/js/base.js
@@ -12,3 +12,12 @@ function setRed() {
     document.querySelector("#textFormatter").classList.remove("green");
     document.querySelector("#textFormatter").classList.add("red");
 }
+
+function submitform() {
+    //validation kontrol edilecek
+    var isValid = true;
+    if (isValid) {
+        document.getElementById("form").submit();
+
+    }
+}
