089bb77b72c833cd6b6a5499496cdec85a159bd3
Hakan LEKESİZ <hakan.lekesiz@baogrenci.com>
2022-11-27 14:04:41 +0300

    11. ders sonu

--- JS/Content/Scripts/main.js
+++ JS/Content/Scripts/main.js
@@ -233,7 +233,9 @@ let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 console.log("string methods");
 console.log("karakter sayısı:" + text.length); //string bir değişkenin karakter sayısnıı dönüyor
 console.log(text.substring(7, 13)); //7. karakterden başlayıp 13. karaktere kadar kesiyor
+//*****
 console.log(text.substring(5)); //5. karakterden başlayıp bitene kadar kesiyor
+//*****
 console.log(text.replace("ABC", "z")); //"ABC" stinrgini "z" ile değiştirir
 
 text = "AFsfsfSfddfAAAAdfdffd"
@@ -248,6 +250,7 @@ let text2 = "World";
 console.log(text1.concat(" ", text2));
 
 text1 = "      Hello World!      ";
+//*****
 console.log(text1.trim());//bir string ifadenin başındaki ve sonundaki tüm boşluluları siler
 console.log(text1.trimStart());
 console.log(text1.trimEnd());
@@ -274,6 +277,123 @@ else {
 
 
 function converToTurkishChar(text) {
-    return text.replaceAll("ğ","g").replaceAll("Ğ","G").replaceAll("İ","I").replaceAll("ı","i").replaceAll("ş","s")
+    return text.replaceAll("ğ", "g").replaceAll("Ğ", "G").replaceAll("İ", "I").replaceAll("ı", "i").replaceAll("ş", "s")
 }
+
+
+//number methods
+
+(123).toString(); //number olan bir değeri stringe çeviriyor
+
+(123.576).toFixed(2);//123.58 ondalıklı kısmı 2 hane olacak şekilde düzelttir ve stringe çevirir
+
+//*****
+Number("10.51"); // 10.51 string ve boolean değerleri rakama çevirir. false
+parseInt("10.51");// 10 string değerleri tam sayıya çevirir 
+parseFloat("10.51");// 10.51
+parseFloat("10");// 10
+
+//arrays
+
+const cars = [
+    "Saab",
+    "Volvo",
+    "BMW"
+];
+
+cars[0] = "Fiat"; // ilk itemın değerini değiştirdik
+cars[cars.length - 1] = "Mercedes"; //son itemın değerini değiştirdik
+cars[cars.length] = "Fiat1"; //yeni değer ataması yapıldı
+cars[cars.length] = "Fiat2";
+cars[cars.length] = "Fiat3";
+cars.push("Fiat4");
+
+for (let i = 0; i < cars.length; i++) {
+    const element = cars[i];
+    console.log(element);
+}
+
+const persons = [
+    { name: "aaa", lastname: "bbb" },
+    { name: "ccc", lastname: "ddd" },
+    { name: "eee", lastname: "fff" },
+];
+
+//*****
+for (let i = 0; i < persons.length; i++) {
+    const person = persons[i];
+    console.log(person.name);
+}
+
+
+
+//array methods
+Array.isArray(cars);//isArray bize cars nesnesinin array olup olmadığı bilgisini verir
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+document.getElementById("main").innerHTML = fruits.toString();
+//result= Banana,Orange,Apple,Mango
+
+document.getElementById("main").innerHTML = fruits.join(" * ");
+//result= Banana * Orange * Apple * Mango
+
+fruits.pop(); //son itemi siliyor
+//*****
+fruits.push("Kiwi");//arrayın sonuna item ekiyor
+fruits.shift();//ilk itemi siliyor
+
+const myGirls = ["Cecilie", "Lone"];
+const myBoys = ["Emil", "Tobias", "Linus"];
+const myChildren = myGirls.concat(myBoys); // iki arayı birleştiriyor
+
+//*****
+fruits.splice(0, 1); //ilk itemi siler
+fruits.splice(1, 1); //ikinci itemdan başlar 1 tane eleman siler
+fruits.splice(1, 2); //ikinci itemdan başlar 2 tane eleman siler
+
+fruits.sort(); // itemları A-Z'ye sıralar
+fruits.reverse(); // itemları tersine çevirir
+
+[40, 100, 1, 5, 25, 10].sort(function (a, b) { return a - b }); //numberları sort eder
+
+//array objesi
+const _cars = [
+    { type: "Volvo", year: 2016, id: "0001" },
+    { type: "Saab", year: 2001, id: "0002" },
+    { type: "BMW", year: 2010, id: "0003" },
+    { type: "Fiat", year: 208, id: "0004" },
+];
+
+ 
+_cars.sort(function (a, b) { return a.year - b.year }); //objenin içindeki numberları sort eder
+
+const numbers = [45, 4, 9, 16, 25];
+
+//18 yaş üzeri numaraları filtreliyoruz
+//*****
+const over18 = numbers.filter(x => x > 18);
+//*****
+const hasOver18 = numbers.some(x => x > 18); //18'den büyük varmı bakar true yada false
+console.log(over18)
+
+//2015 yılı üzeri olan araba modellerini filtreliyoruz
+const over2015Years = _cars.filter(x => x.year > 2015);
+console.log(over2015Years);
+
+//a harfi içeren araba modellerini filtreliyoruz
+const isContainsA = _cars.filter(x => x.type.toLocaleLowerCase().includes("f"));
+console.log(isContainsA);
+
+
+fruits.indexOf("Apple") // array içinde bulduğu ilk itemın sırasını verir
+fruits.lastIndexOf("Apple") // array içinde bulduğu son itemın sırasını verir
+fruits.includes("Mango"); //array içinde mango itemı varmı yokmu diy bakar true yada false döner
+
+//*****
+var selectedCar = _cars.find(x => x.id === "0002"); // array içindeki elemanları filtreler buldğu ilk itemı döner
+console.log(selectedCar);
+
+
+
+
 
