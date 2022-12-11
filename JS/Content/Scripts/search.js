8236b8e6a78def85b9c0a4f8a66b27bfd7096037
Hakan LEKESİZ <hakan.lekesiz@baogrenci.com>
2022-12-11 14:09:43 +0300

    search

--- JS/Content/Scripts/search.js
+++ JS/Content/Scripts/search.js
@@ -8,7 +8,7 @@ var mockData = [
         category: "nft",
         desc: "Faucibus odio elementum nulla venenatis, libero. Ullamcorper duis fringilla pulvinar nibh diam sit.",
         img: "Content/images/home/card.jpg",
-        link:"/urun-detay/1"
+        link: "/urun-detay/1"
 
     },
     {
@@ -16,51 +16,73 @@ var mockData = [
         category: "bitcoin",
         desc: "Bitcoin Faucibus o Bitcoin dio elementum nulla venenatis, libero. Ullamcorper duis fringilla pulvinar nibh diam sit.",
         img: "Content/images/home/card.jpg",
-        link:"/urun-detay/2"
+        link: "/urun-detay/2"
     },
     {
         title: "Moonbirds etherium ",
         category: "etherium",
         desc: "Faucibus odio elementum nulla venenatis, libero. Ullamcorper duis fringilla pulvinar nibh diam sit.",
         img: "Content/images/home/card.jpg",
-        link:"/urun-detay/3"
+        link: "/urun-detay/3"
     },
     {
         title: "etherium sdfsds ds ",
         category: "etherium",
         desc: "Bitcoin Faucibus o Bitcoin dio elementum nulla venenatis, libero. Ullamcorper duis fringilla pulvinar nibh diam sit.",
         img: "Content/images/home/card.jpg",
-        link:"/urun-detay/4"
+        link: "/urun-detay/4"
     },
 
 ]
 
-
 var Search = {
+    filter: {
+        searchParam: "",
+        category:""
+    },
     Elements: {
         searchInput: document.getElementById("input-search"),
+        selectCategory: document.getElementById("select-ctg"),
         results: document.getElementById("search-results"),
         template: document.getElementById("card-template"),
     },
     Action: {
         handleSearch: function () {
+            debugger
             var el = Search.Elements;
+            Search.filter.searchParam = el.searchInput.value;
+            Search.Action.handleFilter();
+
+        },
+        handleCtg: function () {
+            debugger
+            var el = Search.Elements;
+            Search.filter.category = el.selectCategory.value;
+            Search.Action.handleFilter();
+        },
+        handleFilter: function () {
+            debugger
+
+            var el = Search.Elements;
+            var filter = Search.filter;
 
             var filteredData = mockData;
 
-            if (el.searchInput.value.length > 0) {
-                var val = el.searchInput.value.toLocaleLowerCase();
+            if (filter.searchParam.length > 0) {
+                var val = filter.searchParam.toLocaleLowerCase();
 
                 function _filter(data) {
                     return data.title.toLocaleLowerCase().includes(val) || data.desc.toLocaleLowerCase().includes(val)
                 }
 
-                filteredData = mockData.filter(_filter);
+                filteredData = filteredData.filter(_filter);
 
             }
 
-
-            debugger
+            if (filter.category.length > 0) {
+                filteredData = filteredData.filter(x=>x.category === filter.category);
+            }
+ 
             el.results.innerHTML = "";
 
             if (filteredData.length > 0) {
@@ -69,8 +91,8 @@ var Search = {
                     var div = document.createElement("div");
                     div.innerHTML = el.template.innerHTML;
                     debugger
-                    div.querySelector("h5").innerText=element.title;
-                    div.querySelector("p").innerText=element.desc;
+                    div.querySelector("h5").innerText = element.title;
+                    div.querySelector("p").innerText = element.desc;
                     //div.querySelector("img").src= element.img;
                     div.querySelector("img").setAttribute("src", element.img);
                     div.querySelector("a").setAttribute("href", element.link);
@@ -80,11 +102,6 @@ var Search = {
             else {
                 el.results.innerText = "Sonuç Bulunamadı";
             }
-
-
-        },
-        handleCtg:function () {
-            
         }
 
     }
